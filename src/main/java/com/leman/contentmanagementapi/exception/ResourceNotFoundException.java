22b55cfff60580fd5788866196b3804c133884c8
Leman Amrahova <lemrahovaa@gmail.com>
2025-12-25 23:30:48 +0400

    CM-10 FEAT: AOP configuration

--- src/main/java/com/leman/contentmanagementapi/exception/ResourceNotFoundException.java
+++ src/main/java/com/leman/contentmanagementapi/exception/ResourceNotFoundException.java
@@ -1,7 +1,9 @@
 package com.leman.contentmanagementapi.exception;
 
+import com.leman.contentmanagementapi.exception.constant.ErrorMessage;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import java.text.MessageFormat;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -12,7 +14,7 @@ public class ResourceNotFoundException extends RuntimeException {
     private final Object value;
 
     public ResourceNotFoundException(String entity, String field, Object value) {
-        super();
+        super(MessageFormat.format(ErrorMessage.RESOURCE_NOT_FOUND_ERROR_MESSAGE, entity, field, value));
         this.entity = entity;
         this.field = field;
         this.value = value;
